13714c40d7c5a9961be315bcd9279ef803c1a0ef
MNSCORZA <nahusco@gmail.com>
2026-05-06 19:43:17 -0300

    corrijo importacion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router';
 import { Search, ShoppingCart, Menu, X, ChevronRight, ChevronDown } from 'lucide-react';
 import { CartContext } from '../context/CartContext';
 import { db } from '../firebase/config'; 
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'fireBase/firestore';
 import logoImg from '../assets/images/Logo.png';
 
 const MobileMenu = ({ isOpen, setIsOpen, searchValue, setSearchValue, handleSearch, categorias }) => {
