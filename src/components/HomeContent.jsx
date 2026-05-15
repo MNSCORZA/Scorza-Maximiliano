2c4cdd563a51fa9f938519a926af14fdf458d817
MNSCORZA <nahusco@gmail.com>
2026-05-15 19:34:14 -0300

    Actualizar HomeContent.jsx

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Banner } from './Banner';
 import PromoBanner from './PromoBanner';
 import Hero from './Hero';
 import TrustGrid from './TrustGrid';
@@ -9,6 +10,7 @@ import ProductGrid from './ProductGrid';
 const HomeContent = () => {
   return (
     <main className="min-h-screen bg-white text-gray-900 font-sans">
+      <Banner />
       <PromoBanner />
       <Hero />
       <TrustGrid />
