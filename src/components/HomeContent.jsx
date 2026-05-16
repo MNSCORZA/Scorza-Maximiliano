3178d7b93db53640439b131b94e809ee1ebdeb02
MNSCORZA <nahusco@gmail.com>
2026-05-16 18:56:31 -0300

    Actualizar HomeContent.jsx

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+Import React from 'react';
 import { Banner } from './Banner';
 import PromoBanner from './PromoBanner';
 import Hero from './Hero';
@@ -6,30 +6,8 @@ import TrustGrid from './TrustGrid';
 import { CategoriesGrid } from './CategoriesGrid';
 import { FeaturesAndBrands } from './FeaturesAndBrands';
 import ProductGrid from './ProductGrid';
-import { db } from "../fireBase/config";
-import { collection, getDocs, query, limit } from "firebase/firestore";
 
 const HomeContent = () => {
-  const [productos, setProductos] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchHomeProducts = async () => {
-      try {
-        const q = query(collection(db, "productos"), limit(20));
-        const querySnapshot = await getDocs(q);
-        const docs = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setProductos(docs);
-      } catch (error) {
-        console.error(error);
-      } finaly {
-        setLoading(false);
-      }
-    };
-
-    fetchHomeProducts();
-  }, []);
-
   return (
     <main className="min-h-screen bg-white text-gray-900 font-sans">
       <Banner />
@@ -37,8 +15,8 @@ const HomeContent = () => {
       <Hero />
       <TrustGrid />
       <CategoriesGrid />
-      <FeaturesAndBrands productos={productos} />
-      <ProductGrid productos={productos} loading={loading} />
+      <FeaturesAndBrands />
+      <ProductGrid />
     </main>
   );
 };
