fda7ba20978a1d62f751f5d375f687c84b948dcb
MNSCORZA <nahusco@gmail.com>
2026-05-18 12:56:57 -0300

    Crear bannerStyles.js

---
+++ src/constants/bannerStyles.js
@@ -0,0 +1,20 @@
+export const bgStyles = {
+  azul: {
+    top: "from-gray-900 via-indigo-950 to-blue-950",
+    bottom: "bg-blue-600/20",
+    button: "text-gray-900 bg-white hover:bg-orange-500 hover:text-white",
+    subTag: "text-blue-400"
+  },
+  rojo: {
+    top: "from-neutral-950 via-red-950 to-neutral-900",
+    bottom: "bg-red-600/20",
+    button: "text-white bg-red-600 hover:bg-red-700",
+    subTag: "text-red-400"
+  },
+  premium: {
+    top: "from-neutral-950 via-neutral-900 to-neutral-800",
+    bottom: "bg-amber-500/10 border-t lg:border-t-0 lg:border-l border-amber-500/20",
+    button: "text-neutral-900 bg-amber-500 hover:bg-amber-600 font-black",
+    subTag: "text-amber-400"
+  }
+};
